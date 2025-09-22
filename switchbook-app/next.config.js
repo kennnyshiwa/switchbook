5e75b3201344df0ebc5ef01692a29e379a5167d6
kennnyshiwa <kstjohn@kstj.us>
2025-09-21 20:47:41 -0400

    wtf

--- switchbook-app/next.config.js
+++ switchbook-app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://switchbook.app' : undefined,
   images: {
     remotePatterns: [
       {
