416bb9255147693ed1b7cd548fe90755dd73e82f
Kenneth St. John <44236678+kennnyshiwa@users.noreply.github.com>
2026-08-24 00:09:10 -0400

    Make test runner portable in CI

---
+++ switchbook-app/tests/run-tests.ts
@@ -0,0 +1,5 @@
+import "./canonical-switch-share.test";
+import "./client-api-error.test";
+import "./compose-secrets.test";
+import "./master-switch-popup-share.test";
+import "./partner-api.test";
